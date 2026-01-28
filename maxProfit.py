6e62e80db155ebf311de1fd71abed36449b8987e
Vishant <44126013+vishantbhole@users.noreply.github.com>
2026-01-27 22:03:57 -0600

    Add test cases for maxProfit function
    Added test cases to demonstrate maxProfit functionality.

--- maxProfit.py
+++ maxProfit.py
@@ -12,5 +12,9 @@ class Solution:
         return profit
 if __name__ == "__main__":
     sol = Solution()
+    
     prices = [7,1,5,3,6,4]
     print("Output is : ", sol.maxProfit(prices))
+    
+    prices2 = [1,2,3,4,5]
+    print("Output is : ", sol.maxProfit(prices2))
